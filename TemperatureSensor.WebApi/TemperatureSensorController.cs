ddd49ba6ed705503b875f74227c3bf6bf3bce610
Gweenblade <44687370+Gweenblade@users.noreply.github.com>
2022-06-02 20:46:47 +0200

    Working api made by hand.

---
+++ TemperatureSensor.WebApi/TemperatureSensorController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureSensor.WebApi
+{
+    [ApiController]
+    [Route("TemperatureSensor")]
+    public class TemperatureSensorController : ControllerBase
+    {
+        [HttpPost("")]
+        public async Task<IActionResult> CreateTemperatureSensorAsync()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [HttpPatch("")]
+        public async Task<IActionResult> UpdateTemperatureSensorAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("")]
+        public async Task<IActionResult> RemoveTemperatureSensorAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetTemperatureSensorAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("1")]
+        public async Task<IActionResult> GetTemperatureSensorsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
