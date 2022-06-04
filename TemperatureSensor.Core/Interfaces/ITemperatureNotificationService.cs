68bdbe188780e5af04bcc08cc2c87520018e5cb5
Gweenblade <44687370+Gweenblade@users.noreply.github.com>
2022-06-04 11:42:44 +0200

    TemperatureNotificationService

---
+++ TemperatureSensor.Core/Interfaces/ITemperatureNotificationService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureSensor.Core.Interfaces
+{
+    internal interface ITemperatureNotificationService
+    {
+        public Task<bool> SendNotificationToDrive();
+    }
+}
