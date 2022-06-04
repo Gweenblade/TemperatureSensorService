406d06ffffb83fbcfa7afcc4593c3220105cabed
Gweenblade <44687370+Gweenblade@users.noreply.github.com>
2022-06-04 11:40:00 +0200

    Draft of TemperatureSensorService.

---
+++ TemperatureSensor.Core/Interfaces/ITemperatureSensorService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureSensor.Core.InternalModels;
+
+namespace TemperatureSensor.Core.Interfaces
+{
+    interface ITemperatureSensorService
+    {
+        Task<string> GetTemperatureSensorAsync(GetTemperatureSensorModel getTemperatureSensorModel);
+        Task<bool> CreateTemperatureSensorAsync(CreateTemperatureSensorModel createTemperatureSensorModel);
+        Task<bool> RemoveTemperatureSensorAsync(RemoveTemperatureSensorModel removeTemperatureSensorModel);
+        Task<bool> UpdateTemperatureSensorAsync(UpdateTemperatureSensorModel updateTemperatureSensorModel);
+    }
+}
---
+++ TemperatureSensor.Core/Services/TemperatureSensorService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureSensor.Core.Interfaces;
+using TemperatureSensor.Core.InternalModels;
+
+namespace TemperatureSensor.Core.Services
+{
+    internal class TemperatureSensorService : ITemperatureSensorService
+    {
+        public Task<bool> CreateTemperatureSensorAsync(CreateTemperatureSensorModel createTemperatureSensorModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetTemperatureSensorAsync(GetTemperatureSensorModel getTemperatureSensorModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveTemperatureSensorAsync(RemoveTemperatureSensorModel removeTemperatureSensorModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateTemperatureSensorAsync(UpdateTemperatureSensorModel updateTemperatureSensorModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
